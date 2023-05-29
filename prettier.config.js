2386e7d81fc52a9d37eacd355bce1a815ef2f676
Flero <flero@flero.dev>
2023-05-29 20:08:46 +0930

    .config.js ftw

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/// <reference types="prettier" />
+module.exports = {
+	semi: false,
+	singleQuote: false,
+	arrowParens: "always",
+	trailingComma: "all",
+	useTabs: false,
+	tabWidth: 2,
+	bracketSpacing: true,
+	endOfLine: "lf",
+	printWidth: 120,
+	jsxBracketSameLine: true,
+}
